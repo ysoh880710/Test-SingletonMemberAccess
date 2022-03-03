1a48c753b8c4d78c50e885a9439007e4d69ecda9
ysoh880710 <ysoh880710@gmail.com>
2022-03-03 17:13:01 +0900

    프로젝트 생성

---
+++ SingletonMemberAccess/SingletonMemberAccess.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+class CAManager
+{
+private:
+
+    struct SMemory
+    {
+        long m_lA;
+        long m_lB;
+        long m_lC;
+    };
+
+    static CAManager s_Instance;
+
+    SMemory m_stMemory;
+
+    CAManager()
+    {
+    }
+
+    ~CAManager()
+    {
+    }
+
+public:
+
+    static void Set(long _l)
+    {
+        s_Instance.m_stMemory.m_lC = _l;
+    }
+
+    static long Get()
+    {
+        return s_Instance.m_stMemory.m_lC;
+    }
+};
+
+CAManager CAManager::s_Instance{};
+
+int main()
+{
+    sizeof(CAManager);
+
+    CAManager::Set(123L);
+    long lValue = CAManager::Get();
+
+    return 0;
+}
